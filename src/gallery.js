61f54e32b9d705bb6803b94fbd508e0750fa1001
alla karpenko <karpenkoallo4ka@gmail.com>
2021-02-06 01:42:05 +0200

    Обновляем решение задачи

--- src/gallery.js
+++ src/gallery.js
@@ -1,26 +1,19 @@
-import gallery from './gallery-items.js';
+import galleryImages from './gallery-items.js';
 
 const galleryRef = document.querySelector('.gallery');
 const imageBox = document.querySelector('.lightbox__image');
 const openModalBtn = document.querySelector('.lightbox');
 const closeModalBtn = document.querySelector('button[data-action="close-lightbox"]');
-//const slideLeft = document.querySelector('slide-left');
-//const slideRright = document.querySelector('slide-right');
 let idx = 0;
+// const itemRef = document.createElement('li');
+// itemRef.classList.add("gallery__item");
+// const linkRef = document.createElement('a');
+// linkRef.classList.add("gallery__link");
 
-for (let galleria of gallery)
-    galleryRef.insertAdjacentHTML('beforeend', `<li class="gallery__item">
-    <a class="gallery__link" href='${galleria.original}'>
-    <img class="gallery__image" src='${galleria.preview}' data-source='${galleria.original}' alt='${galleria.description}' data-index="${idx}"/></a></li>`);
-console.log(galleryRef);
 
- 
 galleryRef.addEventListener('click', onGalleryClick);
 closeModalBtn.addEventListener('click', onGalleryCloseBtn);
-// galleryRef.addEventListener('keydown', left);
-// galleryRef.addEventListener('keydown', right);
-
-
+ 
 function onGalleryClick(event) {
     event.preventDefault();
 
@@ -28,21 +21,67 @@ function onGalleryClick(event) {
         return;
     }
     const imageRef = event.target;
-    
     const largeImageURL = imageRef.dataset.source;
-    const largeImage = document.querySelector(`.gallery__link`);
    
     openModalBtn.classList.add('is-open');
 
-    largeImage.src = largeImageURL; 
-    imageBox.src = largeImageURL; 
+    setLargeImageSrc(largeImageURL); 
+
+   window.addEventListener('keydown', closeEscape)
+   
 };
+ 
+function setLargeImageSrc(url) {
+    imageBox.src = url;
+}
 
 function onGalleryCloseBtn() {
     window.removeEventListener('click', onGalleryClick);
     openModalBtn.classList.remove('is-open');
-    largeImage.src = "";
+    imageBox.src = "";
 };
+
+galleryImages.forEach((el, idx) => {
+    galleryRef.insertAdjacentHTML('beforeend', `<li class="gallery__item">
+    <a class="gallery__link" href='${el.original}'>
+    <img class="gallery__image" src='${el.preview}' data-source='${el.original}' alt='${el.description}' data-index="${idx}"/></a></li>`);
+})
+console.log(galleryRef);
+
+
+//  /////////////  ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ  //////////////
+ 
+//Закрытие модального окна по нажатию клавиши ESC.//
+
+const closeEscape = e => {
+
+    if (e.code === 'Escape') {
+       openModalBtn.classList.remove('is-open');
+        window.removeEventListener('keydown', closeEscape);
+    }
+};
+
+
+
+
+
+
+// const arr = galleryImages.map((el, idx) => {
+//      `<li class="gallery__item">
+//     <a class="gallery__link" href='${el.original}'>
+//     <img class="gallery__image" src='${el.preview}' data-source='${el.original}' alt='${el.description}' data-index="${idx}"/></a></li>`;
+// })
+// galleryRef.insertAdjacentHTML('beforeend', ...arr)
+
+
+// const markup = galleryImages.reduce((acc, {original, preview, description}, idx) => acc + `<li class="gallery__item"><a class="gallery__link" href='${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" data-index="${idx}"/></a></li>`, '')
+// galleryRef.insertAdjacentHTML('beforeend', markup)
+// console.log(galleryRef);
+
+
+
+
+
 ///////////////
 // function left() {
 //     largeImageURL.map(idx => (idx))
@@ -61,8 +100,6 @@ function onGalleryCloseBtn() {
 //  galleryRef.addEventListener(`click`, () => imageBox.src = Array[idx + 1].original);
 
 //  galleryRef.addEventListener(`click`, () =>  imageBox.src = Array[idx - 1].original);
-//  /////////////  ДОПОЛНИТЕЛЬНОЕ ЗАДАНИЕ  //////////////
-
 
 
 
